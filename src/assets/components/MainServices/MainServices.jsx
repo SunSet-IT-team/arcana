84569994013e2b08f58b53dfefc4e14fd714bf7d
Николай Сердученко <nikserduchenko@yandex.ru>
2025-01-23 19:17:38 +0300

    fix: add price for service

--- src/assets/components/MainServices/MainServices.jsx
+++ src/assets/components/MainServices/MainServices.jsx
@@ -14,18 +14,21 @@ function MainServices() {
             time: '90 – 120 min',
             info: "Se sei stanco di passare da uno psicologo all'altro e di investire inutilmente in corsi di crescita personale, e vuoi prenderecoscienza dei tuoi pattern comportamentali e delle tue convinzioni per liberarti da tutto ciò che ostacola la tua espansione personalee il progresso verso gli obiettivi. Durante la consulenza saràpossibile conoscere sé stessi e comprendere la propria unicità.",
             img: '1',
+            price: 110,
         },
         {
             name: 'RELAZIONI DI COPPIA',
             time: '90 – 120 min',
             info: "Se sogni una relazione armoniosa costruita sulla comprensione reciproca tra i partner, sul rispetto dei propri bisogni e delle aspirazioni per le quali esistono queste relazioni. Comprendere cosa vi unisce e cosa vi divide, come superare l'incomprensione esistente attraverso una profonda consapevolezza dei punti di forza...Se sogni una relazione armoniosa costruita sulla comprensione reciproca tra i partner, sul rispetto dei propri bisogni e delle aspirazioni per le quali esistono queste relazioni. Comprendere cosa vi unisce e cosa vi divide, come superare l'incomprensione esistente attraverso una profonda consapevolezza dei punti di forza...",
             img: '2',
+            price: 110,
         },
         {
             name: 'RAPPORTI SOCIALI',
             time: '60 – 90 min',
             info: 'Come “leggere” gli altri come un libro aperto. Capire perché sorgono conflitti, dove sono le aree problematiche e come comprendere i modelli di comportamento di genitori, capo, colleghi, amici al fine di migliorare la comprensione reciproca e avere relazioni sincere e armoniose.',
             img: '3',
+            price: 110,
         },
     ];
 
