ffdba19e485b11e292990d937e7990d23c3d7806
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2025-04-04 20:07:02 +0300

    fix: change MainInfo text

--- src/assets/components/MainInfo/MainInfo.jsx
+++ src/assets/components/MainInfo/MainInfo.jsx
@@ -14,27 +14,12 @@ function MainInfo() {
 
                     <p className="info__text">
                         La Mappa Archetipica Natale rivela le energie
-                        archetipiche intrinseche alla nostra psiche. È una
-                        rappresentazione simbolica del viaggio della nostra
-                        anima, che mostra i modelli archetipici personali
-                        attraverso i quali percorriamo il cammino
-                        dell’individuazione.
-                        <br /> <br />
-                        Siamo conduttori attraverso cui queste forze si
-                        esprimono, influenzando non solo la nostra vita
-                        interiore, ma anche gli eventi e l’ambiente
-                        circostante.Quando le riconosciamo e le esprimiamo nella
-                        loro forma più autentica, possiamo raggiungere il
-                        benessere interiore, l’autorealizzazione personale e il
-                        successo sociale. <br /> <br /> Il Profilo Archetipico è
-                        una bussola che ti aiuta a comprendere la tua
-                        personalità e il tuo percorso di vita. Rivela le tue
-                        aspirazioni più profonde, fa luce sulle cause delle
-                        difficoltà che incontri e ti offre una prospettiva
-                        chiara su te stesso e sull’ambiente che ti circonda. Ma
-                        soprattutto, non si limita alla conoscenza: è uno
-                        strumento concreto per avviare un processo di
-                        cambiamento positivo e trasformare la tua vita.
+                        archetipiche intrinseche alla nostra psiche che
+                        influenzano le esperienze che attraiamo e l’ambiente che
+                        ci circonda. È una rappresentazione simbolica del
+                        viaggio della nostra anima, che mostra i modelli
+                        archetipici personali attraverso i quali percorriamo il
+                        cammino dell’individuazione.
                     </p>
 
                     <Link
